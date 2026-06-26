faf5f27b02e5f4a19ee33e6a8f7e346356992c12
ARKNET <ogunleye834@gmail.com>
2026-06-26 09:32:11 -0400

    Add src/sanity/schemas/faq.ts

---
+++ src/sanity/schemas/faq.ts
@@ -0,0 +1,9 @@
+export const faq = {
+  name: "faq",
+  title: "FAQ",
+  type: "document",
+  fields: [
+    { name: "question", title: "Question", type: "string" },
+    { name: "answer", title: "Answer", type: "text" },
+  ],
+};
