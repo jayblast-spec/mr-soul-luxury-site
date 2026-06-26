012a64f96621525eb2c76d64571aa7b632dceca9
ARKNET <ogunleye834@gmail.com>
2026-06-26 09:32:12 -0400

    Add src/sanity/schemas/index.ts

---
+++ src/sanity/schemas/index.ts
@@ -0,0 +1,6 @@
+import { faq } from "./faq";
+import { hero } from "./hero";
+import { menuItem } from "./menuItem";
+import { testimonial } from "./testimonial";
+
+export const schemaTypes = [menuItem, faq, hero, testimonial];
