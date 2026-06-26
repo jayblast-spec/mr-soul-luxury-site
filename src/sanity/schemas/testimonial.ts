5e5c4a725cc5e5f9ce3002c447e7e7af1453fdb0
ARKNET <ogunleye834@gmail.com>
2026-06-26 09:32:14 -0400

    Add src/sanity/schemas/testimonial.ts

---
+++ src/sanity/schemas/testimonial.ts
@@ -0,0 +1,11 @@
+export const testimonial = {
+  name: "testimonial",
+  title: "Testimonial",
+  type: "document",
+  fields: [
+    { name: "name", title: "Name", type: "string" },
+    { name: "text", title: "Text", type: "text" },
+    { name: "rating", title: "Rating", type: "number" },
+    { name: "avatar", title: "Avatar", type: "image" },
+  ],
+};
