6f6691854c28f9ba8d37aa8fa96460c292893d53
ARKNET <ogunleye834@gmail.com>
2026-06-26 09:32:12 -0400

    Add src/sanity/schemas/hero.ts

---
+++ src/sanity/schemas/hero.ts
@@ -0,0 +1,12 @@
+export const hero = {
+  name: "hero",
+  title: "Hero",
+  type: "document",
+  fields: [
+    { name: "title", title: "Title", type: "string" },
+    { name: "subtitle", title: "Subtitle", type: "string" },
+    { name: "ctaText", title: "CTA Text", type: "string" },
+    { name: "ctaLink", title: "CTA Link", type: "string" },
+    { name: "backgroundImage", title: "Background Image", type: "image" },
+  ],
+};
