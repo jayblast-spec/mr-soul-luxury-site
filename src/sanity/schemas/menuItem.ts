0542d9ba6e8752ab6bee5d2feec6851245d33318
ARKNET <ogunleye834@gmail.com>
2026-06-26 09:32:13 -0400

    Add src/sanity/schemas/menuItem.ts

---
+++ src/sanity/schemas/menuItem.ts
@@ -0,0 +1,14 @@
+export const menuItem = {
+  name: "menuItem",
+  title: "Menu Item",
+  type: "document",
+  fields: [
+    { name: "name", title: "Name", type: "string" },
+    { name: "description", title: "Description", type: "text" },
+    { name: "price", title: "Price", type: "number" },
+    { name: "category", title: "Category", type: "string" },
+    { name: "image", title: "Image", type: "image" },
+    { name: "isPopular", title: "Popular", type: "boolean" },
+    { name: "isSpicy", title: "Spicy", type: "boolean" },
+  ],
+};
